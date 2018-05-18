6bbe5206b20606237d67070cb580c2f82f2e5f74
christopher crew baker <chriscrewbaker@gmail.com>
2018-05-17 21:03:28 -0400

    add category_expected_rate to fields returned by pull_incites

--- R/process-incites.R
+++ R/process-incites.R
@@ -4,16 +4,16 @@ process_incites <- function(incites_df) {
   cols <- c(
     "isi_loc", "article_type", "tot_cites", "journal_expected_citations",
     "journal_act_exp_citations", "impact_factor", "avg_expected_rate",
-    "percentile", "nci", "esi_most_cited_article", "hot_paper",
-    "is_international_collab", "is_institution_collab", "is_industry_collab",
-    "oa_flag"
+    "category_expected_rate", "percentile", "nci", "esi_most_cited_article",
+    "hot_paper", "is_international_collab", "is_institution_collab",
+    "is_industry_collab", "oa_flag"
   )
   incites_df <- incites_df[, cols]
   colnames(incites_df)[1] <- "ut"
   incites_df$ut <- paste0("WOS:", incites_df$ut)
-  incites_df[, 3:15] <- apply(incites_df[, 3:15], MARGIN = 2, FUN = as.numeric)
-  incites_df[, 10:15] <- apply(
-    incites_df[, 10:15], MARGIN = 2, FUN = function(x) ifelse(x == 1, TRUE, FALSE)
+  incites_df[, 3:16] <- apply(incites_df[, 3:16], MARGIN = 2, FUN = as.numeric)
+  incites_df[, 11:16] <- apply(
+    incites_df[, 11:16], MARGIN = 2, FUN = function(x) ifelse(x == 1, TRUE, FALSE)
   )
   incites_df
 }
