074f4a541865888274d71130a656e1ed88369f8d
Chris Baker <chriscrewbaker@gmail.com>
2018-09-18 15:08:25 -0400

    cast esi_most_cited_article to logical

--- R/process-incites.R
+++ R/process-incites.R
@@ -22,8 +22,8 @@ process_incites <- function(incites_df, as_raw = FALSE) {
   colnames(incites_df)[1] <- "ut"
   incites_df$ut <- paste0("WOS:", incites_df$ut)
   incites_df[, 3:15] <- apply(incites_df[, 3:15], MARGIN = 2, FUN = as.numeric)
-  incites_df[, 11:15] <- apply(
-    incites_df[, 11:15], MARGIN = 2, FUN = function(x) x == 1
+  incites_df[, 10:15] <- apply(
+    incites_df[, 10:15], MARGIN = 2, FUN = function(x) x == 1
   )
   incites_df
 }
