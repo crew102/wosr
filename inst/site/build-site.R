73fe7e49c6fe5f0db9ebc93ba821d3a3ee57c7be
christopher crew baker <chriscrewbaker@gmail.com>
2018-11-01 20:25:46 -0400

    don't rewrite build_rmarkdown_format in pkgdown b/c no longer using paged data frame printing

--- inst/site/build-site.R
+++ inst/site/build-site.R
@@ -1,34 +1,3 @@
-swap_render_fun <- function() {
-
-  # Alter pkgdown:::build_rmarkdown_format
-  build_rmarkdown_format2 <- function(pkg = ".",
-                                      depth = 1L,
-                                      data = list(),
-                                      toc = TRUE) {
-    path <- tempfile(fileext = ".html")
-    suppressMessages(
-      pkgdown::render_page(pkg, "vignette", data, path, depth = depth)
-    )
-    list(
-      path = path,
-      format = rmarkdown::html_document(
-        toc = toc,
-        toc_depth = 2,
-        self_contained = FALSE,
-        theme = NULL,
-        template = path,
-        df_print = "paged", # line added by crew102
-        rownames.print = FALSE
-      )
-    )
-  }
-  # old_fun <- get("build_rmarkdown_format", envir = asNamespace("pkgdown"))
-  # attributes(build_rmarkdown_format2) <- attributes(old_fun)
-  assignInNamespace(
-    "build_rmarkdown_format", build_rmarkdown_format2, ns = "pkgdown"
-  )
-}
-
 build_site <- function() {
   # Render extra vignettes and move vignettes for pkgdown to pick up
   extra_vigs <- list.files(
@@ -38,9 +7,5 @@ build_site <- function() {
   on.exit(try(unlink(x = to, force = TRUE)))
   file.copy(extra_vigs, to = to)
 
-  # Change render function in pkgdown so it uses paged df printing
-  swap_render_fun()
-
-  # Build site
   pkgdown::build_site()
 }
