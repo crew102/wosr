579b0201453d643601f0616f4cfcceb1e0eaad06
Taren Sanders <taren.sanders@acu.edu.au>
2023-05-09 19:53:32 +1000

    Remove a debugging print statement

--- tests/testthat/test-wos.R
+++ tests/testthat/test-wos.R
@@ -4,7 +4,6 @@ test_that("authentication works", {
   skip_if_no_auth()
   # reuse sid across tests, so we don't run into throttling limits. note that
   # this has to be inside test_that function so we can use skip_if_no_auth()
-  print(exists("sid"))
   if (!exists("sid")) sid <<- auth()
   expect_true(is.character(sid))
 })
