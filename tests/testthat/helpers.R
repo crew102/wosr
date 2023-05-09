990ae84b154633d1132953414ab74b2d70d25ee9
Taren Sanders <taren.sanders@acu.edu.au>
2023-05-09 13:12:10 +1000

    Update test harness to use IP authentication

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,6 +1,13 @@
 # Taken from https://cran.r-project.org/web/packages/httr/vignettes/secrets.html
 skip_if_no_auth <- function() {
-  if (identical(Sys.getenv("WOS_USERNAME"), "")) {
+  if (!exists("sid")) {
+    sid_try <- try(auth(username = NULL, password = NULL), silent = TRUE)
+    if (class(sid_try) != "try-error") {
+      assign("sid", sid_try, envir = .GlobalEnv)
+    }
+  }
+
+  if (identical(Sys.getenv("WOS_USERNAME"), "") && class(sid) == "try-error") {
     skip("No authentication available")
   }
 }
